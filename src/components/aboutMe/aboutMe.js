53a4d812f1ff98bc0cca476061c34f336ca8c772
azzahabie <azzahabie@gmail.com>
2021-11-08 23:14:33 +0800

    somewhat perfected the timing

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -11,24 +11,36 @@ export default class AboutMe extends Component {
             myWords: ["HI! I'm Azza!", "I'm currently completing a diploma in Information Technology", "I'm a proud graduate from ITE with a technical diploma in CyberSecurity", "I created my first website back in 2017 but I only got serious in programming in 2020"
                 , "Nowadays I can design website and apps using tools like adobe XD , Photoshop and Illustrtor", "Then I can bring those ideas to life using", "HTML, CSS, JavaScript, React, React Native", "I also handle backend using REST API and connecting to databases such as postgres and mysql"
                 , "then what are your goals?", "I still want to do application development but I am planning to learn AI and machine learning", "I believe these will be the future and I want to contribute in someway", "Anyway, you can see my work.. ill send you links you can click on it", "azzahabie.dev/mywork", "azzahabie.dev/mycontact", "Thank you for your time", "Back to home"],
-            timer: 200,
-            time : 1000
+            timer: 2000,
+            time : 1000,
+            transitionDelay:3000
         }
     }
     componentDidMount() {
         setTimeout(() => {
             this.setState({ animationRemoved: true })
             this.setState({ startAnim: true })
-        }, 3000);
-        this.checkHeight()
+            this.checkHeight()     
+        }, this.state.transitionDelay);
     }
-
+    componentWillUnmount(){
+        clearInterval(this.checkHeight)
+    }
     checkHeight = () => {
-        setInterval(()=>{
-        console.log(`running checkheight`)
+        let localTimer = this.state.time
+        var myInterval = setInterval(()=>{
+        localTimer = localTimer + this.state.timer
+        console.log(localTimer)
         if(document.body.scrollHeight > window.screen.height){
             window.scrollTo(0,document.body.scrollHeight)
-        }}, 1000);}
+        }
+        if(localTimer>this.state.timer*this.state.myWords.length) {clearInterval(myInterval)}
+    }, this.state.timer + 100)
+    }
+
+    clear = () => {
+        clearInterval(this.checkHeight)
+    }
 
     render() {
         return (
